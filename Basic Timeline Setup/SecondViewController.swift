382f1f8abd50c4871c0fa1a226879a692342eb9d
Chad-Michael Muirhead <chadmuirhead@live.com>
2019-07-21 11:39:36 -0400

    Initial Commit

---
+++ Basic Timeline Setup/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Basic Timeline Setup
+//
+//  Created by Di_Nerd on 7/21/19.
+//  Copyright © 2019 Di_Nerd. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
